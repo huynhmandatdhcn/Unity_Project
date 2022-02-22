e3522dedfa10dce74b42d02fa58628dd2a87ee77
huynhmandatdhcn <huynhmandatdhcn@gmail.com>
2022-02-22 10:59:46 +0700

    huynhmanndatdhcn

---
+++ Adventure/Assets/Scripts/MoveByKey.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByKey : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public float runSpeed;
+    public float jumpSpeed;
+    public LayerMask groundLayers;
+    public Transform body;
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRunning();
+        UpdateJumping();
+        UpdateFacing();
+    }
+
+    private void UpdateRunning()
+    {
+        var xInput = Input.GetAxis("Horizontal");
+        var newVelocity = rigid.velocity;
+        newVelocity.x = xInput * runSpeed;
+        rigid.velocity = newVelocity;
+    }
+
+    private void UpdateJumping()
+    {
+        if (JumpButtonPressed && IsOnGround())
+        {
+            var newVelocity = rigid.velocity;
+            newVelocity.y = jumpSpeed;
+            rigid.velocity = newVelocity;
+        }
+    }
+
+    private void UpdateFacing()
+    {
+        var yAngle = (rigid.velocity.x < 0) ? 180 : 0;
+        body.localRotation = Quaternion.Euler(0, yAngle, 0);
+    }
+
+    private bool JumpButtonPressed => Input.GetAxis("Jump") == 1;
+
+    private bool IsOnGround() 
+        => Physics2D.OverlapCircle(transform.position, 0.1f, groundLayers) != null;
+
+    private void OnValidate() => rigid = GetComponent<Rigidbody2D>();
+}
